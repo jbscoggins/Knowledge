e7770c24c39cd677ec6b9f2ba5a7b6fdc5bee90a
jbscoggins <jscogg444@yahoo.com>
2017-05-10 13:49:04 -0700

    Update Lab3_2_BusinessRule.js

--- Lab3_2_BusinessRule.js
+++ Lab3_2_BusinessRule.js
@@ -1,6 +1,6 @@
 (function executeRule(current, previous /*null when async*/) {
 	try {
-		var r = new sn_ws.RESTMessageV2('x_92516_k17_slack.Lab 3.0', 'Lab 3.0 POST');
+		var r = new sn_ws.RESTMessageV2('x_snc_k17_slack.Lab 3.0', 'Lab 3.0 POST');
 		r.setStringParameter('username', 'Lab3.0');
 		r.setStringParameter('number', current.number);
 		r.setStringParameter('title', 'New Priority ' + current.priority + ' Incident has been created');
